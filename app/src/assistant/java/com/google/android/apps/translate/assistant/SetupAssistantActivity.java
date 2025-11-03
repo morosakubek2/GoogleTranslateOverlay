02e310e5da9ae98343fa7df86913a6aa559f665a
morosakubek2 <132716550+morosakubek2@users.noreply.github.com>
2025-11-04 00:08:38 +0100

    Update SetupAssistantActivity.java

--- app/src/assistant/java/com/google/android/apps/translate/assistant/SetupAssistantActivity.java
+++ app/src/assistant/java/com/google/android/apps/translate/assistant/SetupAssistantActivity.java
@@ -2,14 +2,13 @@ package com.google.android.apps.translate.assistant;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.provider.Settings;
 import android.os.Bundle;
+import android.provider.Settings;
 
 public class SetupAssistantActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Otwórz ustawienia, by użytkownik mógł ustawić jako domyślny asystent
         startActivity(new Intent(Settings.ACTION_VOICE_INPUT_SETTINGS));
         finish();
     }
