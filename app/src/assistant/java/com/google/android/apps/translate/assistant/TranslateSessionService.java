dc814db8684fdd250a6b49bc49e7fea1b52e43ec
morosakubek2 <132716550+morosakubek2@users.noreply.github.com>
2025-11-05 18:24:49 +0100

    Update TranslateSessionService.java

--- app/src/assistant/java/com/google/android/apps/translate/assistant/TranslateSessionService.java
+++ app/src/assistant/java/com/google/android/apps/translate/assistant/TranslateSessionService.java
@@ -3,11 +3,13 @@ package com.google.android.apps.translate.assistant;
 import android.os.Bundle;
 import android.service.voice.VoiceInteractionSession;
 import android.service.voice.VoiceInteractionSessionService;
+import android.util.Log;
 
 public class TranslateSessionService extends VoiceInteractionSessionService {
 
     @Override
     public VoiceInteractionSession onNewSession(Bundle args) {
+        Log.d("GOTr", "Creating new TranslateSession");
         return new TranslateSession(this);
     }
 }
