595e14f9369961fb4cc5f8b52f7c1c6691de46dc
morosakubek2 <132716550+morosakubek2@users.noreply.github.com>
2025-11-05 04:13:02 +0100

    Update VoiceAssistantService.java

--- app/src/assistant/java/com/google/android/apps/translate/assistant/VoiceAssistantService.java
+++ app/src/assistant/java/com/google/android/apps/translate/assistant/VoiceAssistantService.java
@@ -1,5 +1,6 @@
 package com.google.android.apps.translate.assistant;
 
+import android.content.Intent;
 import android.service.voice.VoiceInteractionService;
 
 public class VoiceAssistantService extends VoiceInteractionService {
@@ -7,6 +8,11 @@ public class VoiceAssistantService extends VoiceInteractionService {
     @Override
     public void onReady() {
         super.onReady();
-        // Serwis jest gotowy do pracy
+        // Serwis jest gotowy - Android automatycznie używa sessionService z XML
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
     }
 }
