074286d66ddce4e4a56c02df8fdea5bacd84332f
morosakubek2 <132716550+morosakubek2@users.noreply.github.com>
2025-11-05 16:28:53 +0100

    Update TranslateActivity.java

--- app/src/main/java/com/google/android/apps/translate/TranslateActivity.java
+++ app/src/main/java/com/google/android/apps/translate/TranslateActivity.java
@@ -16,7 +16,7 @@ public class TranslateActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("GOTranslate", "TranslateActivity started");
+        Log.d("GOTr", "TranslateActivity started");
 
         Intent incomingIntent = getIntent();
         if (incomingIntent != null) {
@@ -26,7 +26,7 @@ public class TranslateActivity extends Activity {
     }
 
     private void redirectToOffline(Intent incomingIntent) {
-        Log.d("GOTranslate", "Redirecting to offline translator");
+        Log.d("GOTr", "Redirecting to offline translator");
         
         Intent offlineIntent = new Intent(incomingIntent);
         offlineIntent.setComponent(new ComponentName(OFFLINE_PACKAGE, OFFLINE_PACKAGE + OFFLINE_MAIN_ACTIVITY));
@@ -37,24 +37,24 @@ public class TranslateActivity extends Activity {
                 String sharedText = incomingIntent.getStringExtra(Intent.EXTRA_TEXT);
                 if (!TextUtils.isEmpty(sharedText)) {
                     offlineIntent.putExtra(Intent.EXTRA_TEXT, sharedText);
-                    Log.d("GOTranslate", "Sending text: " + sharedText);
+                    Log.d("GOTr", "Sending text: " + sharedText);
                 }
             } else if (incomingIntent.getType() != null && incomingIntent.getType().startsWith("image/")) {
                 Uri imageUri = incomingIntent.getParcelableExtra(Intent.EXTRA_STREAM);
                 if (imageUri != null) {
                     offlineIntent.putExtra(Intent.EXTRA_STREAM, imageUri);
-                    Log.d("GOTranslate", "Sending image: " + imageUri);
+                    Log.d("GOTr", "Sending image: " + imageUri);
                 }
             }
         } else if ("android.intent.action.TRANSLATE".equals(action)) {
             String textToTranslate = incomingIntent.getStringExtra(Intent.EXTRA_TEXT);
             if (!TextUtils.isEmpty(textToTranslate)) {
                 offlineIntent.putExtra(Intent.EXTRA_TEXT, textToTranslate);
-                Log.d("GOTranslate", "Translating text: " + textToTranslate);
+                Log.d("GOTr", "Translating text: " + textToTranslate);
             }
         }
 
         startActivity(offlineIntent);
-        Log.d("GOTranslate", "Successfully started offline translator");
+        Log.d("GOTr", "Successfully started offline translator");
     }
 }
