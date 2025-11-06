32767a07bd6d740cd8d289af6e165f22a55e64a9
morosakubek2 <132716550+morosakubek2@users.noreply.github.com>
2025-11-06 16:27:24 +0100

    Update VoiceAssistantService.java

--- app/src/assistant/java/com/google/android/apps/translate/assistant/VoiceAssistantService.java
+++ app/src/assistant/java/com/google/android/apps/translate/assistant/VoiceAssistantService.java
@@ -2,7 +2,6 @@ package com.google.android.apps.translate.assistant;
 
 import android.service.voice.VoiceInteractionService;
 import android.util.Log;
-import android.content.Intent;
 
 public class VoiceAssistantService extends VoiceInteractionService {
     private static final String TAG = "GOTr";
@@ -23,21 +22,5 @@ public class VoiceAssistantService extends VoiceInteractionService {
     public void onShutdown() {
         super.onShutdown();
         Log.d(TAG, "VoiceAssistantService shutdown");
-    }
-
-    // Gdy asystent jest wywołany, aktywujemy AccessibilityService
-    @Override
-    public void onLaunchVoiceAssistFromKeyguard() {
-        Log.d(TAG, "Voice assist launched from keyguard - activating translation");
-        activateTranslationAssistant();
-    }
-
-    private void activateTranslationAssistant() {
-        Log.d(TAG, "Sending broadcast to activate accessibility service");
-        
-        // Wysyłamy broadcast do AccessibilityService
-        Intent intent = new Intent("ACTIVATE_TRANSLATION_ASSISTANT");
-        intent.setPackage(getPackageName());
-        sendBroadcast(intent);
     }
 }
