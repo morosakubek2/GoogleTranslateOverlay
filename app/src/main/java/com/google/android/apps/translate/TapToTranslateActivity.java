4d124863ff2ec1c6c8c4e50de375173743f10afd
morosakubek2 <132716550+morosakubek2@users.noreply.github.com>
2025-11-06 13:49:41 +0100

    Update TapToTranslateActivity.java

--- app/src/main/java/com/google/android/apps/translate/TapToTranslateActivity.java
+++ app/src/main/java/com/google/android/apps/translate/TapToTranslateActivity.java
@@ -1,59 +1,81 @@
-package com.google.android.apps.translate.assistant;
+package com.google.android.apps.translate.copydrop.gm3;
 
+import android.app.Activity;
 import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.service.voice.VoiceInteractionSession;
+import android.text.TextUtils;
 import android.util.Log;
 
-public class TranslateSession extends VoiceInteractionSession {
+public class TapToTranslateActivity extends Activity {
 
-    public TranslateSession(Context context) {
-        super(context);
-        Log.d("GOTr", "TranslateSession created");
-    }
+    private static final String OFFLINE_PACKAGE = "dev.davidv.translator";
+    private static final String OFFLINE_PROCESS_ACTIVITY = ".ProcessTextActivity";
 
     @Override
-    public void onShow(Bundle args, int showFlags) {
-        super.onShow(args, showFlags);
-        Log.d("GOTr", "onShow called with flags: " + showFlags);
-    }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("GOTr", "TapToTranslateActivity started");
 
-    @Override
-    public void onHandleAssist(Bundle data, android.app.assist.AssistStructure structure, android.app.assist.AssistContent content) {
-        Log.d("GOTr", "=== ASSIST START ===");
-        
-        Log.d("GOTr", "Triggering copy to clipboard");
-        boolean copySuccess = performGlobalAction(GLOBAL_ACTION_COPY);
-        
-        if (copySuccess) {
-            Log.d("GOTr", "Copy successful - launching translation from clipboard");
-            launchTranslation();
-        } else {
-            Log.d("GOTr", "Copy failed - cannot proceed");
+        Intent incomingIntent = getIntent();
+        if (incomingIntent != null) {
+            processIncomingIntent(incomingIntent);
         }
-        
         finish();
     }
 
-    private void launchTranslation() {
-        Intent processTextIntent = new Intent(Intent.ACTION_PROCESS_TEXT);
-        processTextIntent.setType("text/plain");
-        processTextIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        
-        processTextIntent.setComponent(new ComponentName(
-            getContext().getPackageName(),
-            "com.google.android.apps.translate.copydrop.gm3.TapToTranslateActivity"
-        ));
-        
-        getContext().startActivity(processTextIntent);
-        Log.d("GOTr", "Launched PROCESS_TEXT for clipboard translation");
+    private void processIncomingIntent(Intent incomingIntent) {
+        String action = incomingIntent.getAction();
+        Log.d("GOTr", "Action: " + action);
+
+        if (Intent.ACTION_PROCESS_TEXT.equals(action)) {
+            String text = incomingIntent.getStringExtra(Intent.EXTRA_PROCESS_TEXT);
+            
+            if (TextUtils.isEmpty(text)) {
+                Log.d("GOTr", "PROCESS_TEXT with empty text - launching translator for clipboard");
+                launchOfflineTranslator(null);
+            } else {
+                Log.d("GOTr", "PROCESS_TEXT with text: " + text);
+                launchOfflineTranslator(text);
+            }
+        } else {
+            Log.d("GOTr", "Unhandled intent action: " + action);
+            redirectToOffline(incomingIntent);
+        }
     }
 
-    @Override
-    public void onHide() {
-        super.onHide();
-        Log.d("GOTr", "TranslateSession hidden");
+    private void launchOfflineTranslator(String text) {
+        Intent offlineIntent = new Intent(Intent.ACTION_PROCESS_TEXT);
+        offlineIntent.setComponent(new ComponentName(OFFLINE_PACKAGE, OFFLINE_PACKAGE + OFFLINE_PROCESS_ACTIVITY));
+        offlineIntent.setType("text/plain");
+        
+        if (!TextUtils.isEmpty(text)) {
+            offlineIntent.putExtra(Intent.EXTRA_PROCESS_TEXT, text);
+        }
+        
+        offlineIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        startActivity(offlineIntent);
+        Log.d("GOTr", "Successfully started offline translator with text: " + (TextUtils.isEmpty(text) ? "FROM_CLIPBOARD" : text));
+    }
+
+    private void redirectToOffline(Intent incomingIntent) {
+        Log.d("GOTr", "Redirecting to offline translator (PROCESS_TEXT)");
+        
+        Intent offlineIntent = new Intent(incomingIntent);
+        offlineIntent.setComponent(new ComponentName(OFFLINE_PACKAGE, OFFLINE_PACKAGE + OFFLINE_PROCESS_ACTIVITY));
+
+        String action = incomingIntent.getAction();
+        if ("android.intent.action.PROCESS_TEXT".equals(action) || "android.intent.action.PROCESS_TEXT_READONLY".equals(action)) {
+            String processText = incomingIntent.getStringExtra(Intent.EXTRA_PROCESS_TEXT);
+            if (!TextUtils.isEmpty(processText)) {
+                offlineIntent.putExtra(Intent.EXTRA_PROCESS_TEXT, processText);
+                offlineIntent.putExtra(Intent.EXTRA_PROCESS_TEXT_READONLY, incomingIntent.getBooleanExtra(Intent.EXTRA_PROCESS_TEXT_READONLY, false));
+                Log.d("GOTr", "Processing text: " + processText);
+            }
+        }
+
+        startActivity(offlineIntent);
+        Log.d("GOTr", "Successfully started offline translator (PROCESS_TEXT)");
     }
 }
