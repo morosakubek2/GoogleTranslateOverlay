b9c87da414bc4742403b47664fb7e9f96647cba3
morosakubek2 <132716550+morosakubek2@users.noreply.github.com>
2025-11-06 14:52:35 +0100

    Update VoiceAssistantService.java

--- app/src/assistant/java/com/google/android/apps/translate/assistant/VoiceAssistantService.java
+++ app/src/assistant/java/com/google/android/apps/translate/assistant/VoiceAssistantService.java
@@ -1,44 +1,48 @@
 package com.google.android.apps.translate.assistant;
 
-import android.app.KeyguardManager;
+import android.content.ComponentName;
 import android.content.Context;
-import android.os.PowerManager;
+import android.content.Intent;
 import android.service.voice.VoiceInteractionService;
 import android.util.Log;
 
 public class VoiceAssistantService extends VoiceInteractionService {
+    private static final String TAG = "GOTr";
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d("GOTr", "VoiceAssistantService created");
+        Log.d(TAG, "VoiceAssistantService created");
     }
 
     @Override
     public void onReady() {
         super.onReady();
-        Log.d("GOTr", "VoiceAssistantService ready");
+        Log.d(TAG, "VoiceAssistantService ready");
         setDisabledShowContext(0);
     }
 
     @Override
-    public boolean onSupportsAssist() {
-        return true;
-    }
-
-    @Override
-    public boolean onSupportsLaunchVoiceAssistFromKeyguard() {
-        return false;
-    }
-
-    @Override
     public void launchVoiceAssistFromKeyguard() {
-        Log.d("GOTr", "Voice assist from keyguard blocked");
+        Log.d(TAG, "Launching voice assist from keyguard");
+        activateAccessibilityService();
+        super.launchVoiceAssistFromKeyguard();
     }
 
     @Override
     public void onShutdown() {
         super.onShutdown();
-        Log.d("GOTr", "VoiceAssistantService shutdown");
+        Log.d(TAG, "VoiceAssistantService shutdown");
+    }
+
+    private void activateAccessibilityService() {
+        try {
+            Intent intent = new Intent(this, TranslateAccessibilityService.class);
+            intent.setAction("ACTIVATE_ASSISTANT");
+            startService(intent);
+            Log.d(TAG, "Accessibility service activation requested");
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to activate accessibility service", e);
+        }
     }
 }
